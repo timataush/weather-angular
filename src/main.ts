a5603ab17fdec66bbdcc5854a1f0b2cc713d69bd
timataush <17.jun06.2005@gmail.com>
2025-06-24 14:05:49 +0300

    18 версия

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
